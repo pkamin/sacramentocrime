0e58ce07bcd003d3272f4142cedbc6e6df20634d
kamyk <p_kaminski@poczta.onet.pl>
2013-03-12 23:29:22 +0100

    mapreduce

--- mapreduce.rb
+++ mapreduce.rb
@@ -1,23 +1,25 @@
+require 'rubygems'
+require 'mongo'
+
+include Mongo
+
+#var map = function() { emit(this.district, 1); }
+#var reduce = function(key, values) { var total = 0; for (index in values) { total += 1; } return total;}
+#db.crimes.mapReduce(map, reduce, { out: "map_reduce_example" })
+
 def mongoMapReduce()
   @client = MongoClient.new('localhost', 27017)
   @db     = @client['test']
-  @coll   = @db['crimes']
-  map = "function() { emit(this.cdatetime, {district: this.district}); }"
-  reduce = "function(key, values) { " +
-  "var sum = 0; " +
-  "values.forEach(function(doc) { " +
-  " sum += doc.district; " +
-  "}); " +
-  "return {district: sum}; " +
-  "};"
-  @results = @client.map_reduce(map, reduce, :out => "mr_results")
+  @crimes   = @db['crimes']
+  map = "function() { emit(this.district, 1); }"
+  reduce = "function(key, values) { var total = 0; for (index in values) { total += values[index]; } return total;}"
+  @results = @crimes.map_reduce(map, reduce, :out => "mr_results")
   puts "ok"
-  @results.find()
+  @results.find().each { |item| puts item }
 end
 
 def couchDBMapReduce()
-  map = "function() {" + 
-    + "emit(this.author, {votes: this.votes}); }"
+  map = "function() { emit(this.author, {votes: this.votes}); }"
   reduce = "function(key, values) { " +
   "var sum = 0; " +
   "values.forEach(function(doc) { " +
@@ -25,8 +27,8 @@ def couchDBMapReduce()
   "}); " +
   "return {votes: sum}; " +
   "};"
-  @results = @comments.map_reduce(map, reduce, :out => "mr_results")
-  @results.find().to_a  
+  #@results = @comments.map_reduce(map, reduce, :out => "mr_results")
+  #@results.find().to_a  
 end
 
 mongoMapReduce()
